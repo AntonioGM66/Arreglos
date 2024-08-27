aa7cb0932d85395ec6981798ca97fa58de7a4d6a
AntonioGM66 <oitog3@gmail.com>
2024-08-27 17:09:17 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+/*let Array = [];
+
+for (let i = 0; i < 10; i++) 
+{
+    // Generar un número aleatorio entre 0 y 100 (puedes ajustar el rango)
+    Array[i] =Math.floor(Math.random() * 101);
+}
+
+console.log(Array);
+
+let String = prompt("Ingresa varios valores separados por comas:");
+
+let array = String.split(",");
+
+console.log(array);
+
+let array = [10, 40, 30, 20, 15, 5];
+
+// Ordenar el array de menor a mayor, creando un nuevo arreglo con slice
+let arrayOrdenado = array.slice().sort((a, b) => a - b);
+
+// Encontrar el número menor y el número mayor
+let numeroMenor = Math.min(...array);
+let numeroMayor = Math.max(...array);
+
+console.log("Arreglo ordenado de menor a mayor:", arrayOrdenado);
+console.log("Número menor:", numeroMenor);
+console.log("Número mayor:", numeroMayor);*/
